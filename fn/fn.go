2d83a3fb614d85ab1d014eab96513b890af2a7b2
frank <274510919@qq.com>
2024-08-06 13:26:44 +0800

    ...

--- fn/fn.go
+++ fn/fn.go
@@ -1,5 +1,21 @@
-package fn 
+package fn
 
-type Fn struct {
-	
+type Fun[U, T any] func(x ...U) T
+
+type Fn[F Fun[U, T], U, T any] struct {
+	param []U
+	fun   F
+}
+
+func New[F Fun[U, T], U, T any](f F) *Fn[F, U, T] {
+	return &Fn[F, U, T]{fun: f}
+}
+
+func (f *Fn[F, U, T]) Partial(x ...U) *Fn[F, U, T] {
+	f.param = x
+	return f
+}
+
+func (f *Fn[F, U, T]) Call(x ...U) T {
+	return f.fun(append(f.param, x...)...)
 }
