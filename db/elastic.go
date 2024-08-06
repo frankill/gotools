060d2a53b2a50fd8178e55a4ce3c8affed8b6c33
frank <274510919@qq.com>
2024-08-06 11:36:55 +0800

    ...

---
+++ db/elastic.go
@@ -0,0 +1,108 @@
+package db
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"strconv"
+	"sync"
+
+	"github.com/frankill/gotools/array"
+	"github.com/olivere/elastic/v7"
+)
+
+// 定义一个类型，添加 Index 和 ReturnFields 字段
+type ElasticSearchClient[T any] struct {
+	Client       *elastic.Client
+	Index        string
+	Query        elastic.Query
+	ReturnFields []string // 添加 ReturnFields 字段
+}
+
+// 创建 ElasticSearchClient 实例的工厂函数
+func NewElasticSearchClient[T any](client *elastic.Client, index string, query elastic.Query, returnFields []string) *ElasticSearchClient[T] {
+	return &ElasticSearchClient[T]{
+		Client:       client,
+		Index:        index,
+		Query:        query,
+		ReturnFields: returnFields,
+	}
+}
+
+// 查询分片数量并执行 Query
+func (es *ElasticSearchClient[T]) QueryAnyIter() (chan T, error) {
+
+	stringChan := make(chan T, 100)
+
+	var wg sync.WaitGroup
+
+	go func() {
+		defer close(stringChan)
+
+		slice, err := es.Client.IndexGetSettings(es.Index).Do(context.Background())
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		num, _ := strconv.Atoi(slice[es.Index].Settings["index"].(map[string]interface{})["number_of_shards"].(string))
+
+		shardIDs := array.ArraySeq(0, num, 1)
+
+		for _, shardID := range shardIDs {
+			wg.Add(1)
+
+			go func(shardID int) {
+				defer wg.Done()
+
+				svc := es.Client.Scroll(es.Index).KeepAlive("30m").Size(10000).
+					Query(es.Query).
+					Slice(elastic.NewSliceQuery().Id(shardID).Max(num)).FetchSourceContext(
+					elastic.NewFetchSourceContext(true).Include(es.ReturnFields...),
+				)
+
+				defer svc.Clear(context.Background())
+
+				for {
+					res, err := svc.Do(context.Background())
+
+					if err == io.EOF {
+						break
+					}
+					if err != nil {
+						break
+					}
+					if res == nil {
+						break
+					}
+					if res.Hits == nil {
+						break
+					}
+					if res.Hits.TotalHits.Value == 0 {
+						break
+					}
+					for _, hit := range res.Hits.Hits {
+						var results T
+						err := json.Unmarshal(hit.Source, &results)
+						if err != nil {
+							log.Println(err)
+							continue
+						}
+
+						stringChan <- results
+					}
+
+				}
+
+			}(shardID)
+		}
+
+		// 等待所有分片查询完成
+		wg.Wait()
+	}()
+
+	// 返回通道和 nil 错误
+	return stringChan, nil
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,16 @@ go 1.22
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/olivere/elastic/v7 v7.0.32
 	github.com/xuri/excelize/v2 v2.8.1
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
