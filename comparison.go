ba7fa4090c512206d2ea2c1a882dd48a2132fe09
frank <274510919@qq.com>
2024-12-06 16:05:17 +0800

    ...

--- comparison.go
+++ comparison.go
@@ -2,99 +2,36 @@ package gotools
 
 import "fmt"
 
-func GtFloat64(x, y float64) bool {
+func Cmp[T Ordered](x, y T) int {
+	if x > y {
+		return 1
+	} else if x < y {
+		return -1
+	}
+	return 0
+}
+
+func Gt[T Ordered](x, y T) bool {
 	return x > y
 }
 
-func GteFloat64(x, y float64) bool {
-	return x >= y
-}
-
-func LtFloat64(x, y float64) bool {
-	return x < y
-}
-
-func LteFloat64(x, y float64) bool {
-	return x <= y
-}
-
-func EqFloat64(x, y float64) bool {
+func Eq[T Ordered](x, y T) bool {
 	return x == y
 }
 
-func NotEqFloat64(x, y float64) bool {
-	return x != y
-}
-
-func GtFloat32(x, y float32) bool {
-	return x > y
-}
-
-func GteFloat32(x, y float32) bool {
-	return x >= y
-}
-
-func LtFloat32(x, y float32) bool {
-	return x < y
-}
-
-func LteFloat32(x, y float32) bool {
+func Lte[T Ordered](x, y T) bool {
 	return x <= y
 }
 
-func EqFloat32(x, y float32) bool {
-	return x == y
-}
-
-func NotEqFloat32(x, y float32) bool {
-	return x != y
-}
-
-func LtInt(x, y int) bool {
+func Lt[T Ordered](x, y T) bool {
 	return x < y
 }
 
-func EqInt(x, y int) bool {
-	return x == y
-}
-
-func GtInt(x, y int) bool {
-	return x > y
-}
-
-func GteInt(x, y int) bool {
+func Gte[T Ordered](x, y T) bool {
 	return x >= y
 }
 
-func LteInt(x, y int) bool {
-	return x <= y
-}
-
-func EqStr(x, y string) bool {
-	return x == y
-}
-
-func NotEqInt(x, y string) bool {
-	return x != y
-}
-
-func Gtstr(x, y string) bool {
-	return x > y
-}
-
-func GteStr(x, y string) bool {
-	return x >= y
-}
-
-func LtStr(x, y string) bool {
-	return x < y
-}
-
-func LteStr(x, y string) bool {
-	return x <= y
-}
-
-func NotEqStr(x, y string) bool {
+func NotEq[T Ordered](x, y T) bool {
 	return x != y
 }
 
