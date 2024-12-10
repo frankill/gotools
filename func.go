0441aaab934f08015a45387c8cb0bca6ec8869f3
frank <274510919@qq.com>
2024-12-10 16:11:26 +0800

    ...

--- func.go
+++ func.go
@@ -9,6 +9,40 @@ func Cmp[T Ordered](x, y T) int {
 	return 0
 }
 
+func Add[T Number](x, y T) T {
+	return x + y
+}
+
+func Sub[T Number](x, y T) T {
+	return x - y
+}
+
+func Mul[T Number](x, y T) T {
+	return x * y
+}
+
+func Div[T Number](x, y T) T {
+	return x / y
+}
+
+func Mod[T Integer](x, y T) T {
+	return x % y
+}
+
+func Min[T Ordered](x, y T) T {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max[T Ordered](x, y T) T {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func Gt[T Ordered](x, y T) bool {
 	return x > y
 }
